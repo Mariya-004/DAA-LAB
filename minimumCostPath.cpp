20820532aa55f1d371c420bb554d2ed8e09efc3b
Mariya-004 <mariyajyothy04@gmail.com>
2024-10-28 15:15:14 +0530

    Update minimumCostPath.cpp

--- minimumCostPath.cpp
+++ minimumCostPath.cpp
@@ -8,30 +8,41 @@ void display(int mat[4][3]){
 		cout<<endl;
 	}
 }
-int coinCollect(int c[4][3]){
+int minimumCostPath(int c[4][3]){
 	int f[4][3];
-	for (int i=0;i<4;i++){
+	for (int i=0;i<4;i++){                            //copying the matrix
 		for (int j=0;j<3;j++){
 			f[i][j]=c[i][j];
 		}
 	}
-		for (int j=1;j<3;j++){
+		for (int j=1;j<3;j++){            //first row of the matrix
 		f[0][j]=c[0][j]+f[0][j-1];
 	}
-	for (int i=1;i<4;i++){
+	for (int i=1;i<4;i++){                           //first column of the matrix
 		f[i][0]=c[i][0]+f[i-1][0];
 	}
+	int arr[4];
+	int row=-1;
+	int col=-1;
+	arr[
 	for (int i=1;i<4;i++){
 		for(int j=1;j<3;j++){
 			int temp=1000;
 			if (f[i][j-1]<f[i-1][j]){
 				temp=f[i][j-1];
+				row=i;
+				col=j-1;
 			}
 			else{
 				temp=f[i-1][j];
+				row=i-1;
+				col=j;
+			
 			}
 			if (temp>f[i-1][j-1]){
 			    temp=f[i-1][j-1];
+			    row=i-1;
+			    col=j-1;
 			}
 			f[i][j]=temp+c[i][j];
 		}
@@ -48,8 +59,15 @@ int inp[4][3]={{3, 2, 8},
 
 	display(inp);
 	cout<<endl;
-	cout<<"Maximum number of coins collected is:"<<coinCollect(inp);
+	minimumCostPath(inp);
 }
+	
+		
+	
+		
+	
+			
+				
 	
 		
 	
