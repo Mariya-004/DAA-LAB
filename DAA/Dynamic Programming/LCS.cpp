3ac0845536db5a324600e4e2cc324463c68750d6
Mariya-004 <mariyajyothy04@gmail.com>
2024-11-05 17:35:39 +0530

    Update LCS.cpp

--- DAA/Dynamic Programming/LCS.cpp
+++ DAA/Dynamic Programming/LCS.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
-#include<cstring>
 using namespace std;
 int max(int a,int b){
-	if (a>b){
+	if(a>b){
 		return a;
 	}
 	else{
@@ -13,12 +12,12 @@ int lcs(string s1,string s2){
 	int rows=s1.length()+1;
 	int cols=s2.length()+1;
 	int mat[rows][cols];
-	for (int i=0;i<=rows;i++){
+	for (int i=0;i<rows;i++){
 		mat[i][0]=0;
 	}
-	for (int i=0;i<=cols;i++){
+	for (int i=0;i<cols;i++){
 		mat[0][i]=0;
-	}
+	}	
 	for (int i=1;i<rows;i++){
 		for (int j=1;j<cols;j++){
 			if (s1[i-1]==s2[j-1]){
@@ -29,14 +28,38 @@ int lcs(string s1,string s2){
 			}
 		}
 	}
-	for (int i=0;i<rows;i++){
+		for (int i=0;i<rows;i++){
 		for (int j=0;j<cols;j++){
-			cout<<mat[i][j]<<"\t";
+				cout<<mat[i][j]<<" ";
 		}
 		cout<<endl;
 	}
+	int s=mat[rows-1][cols-1];
+	char arr[s+1];
+	int r=rows-1;
+	int c=cols-1;
+	int d=s-1;
+	arr[s]='\0';
+	while (r>0 and c>0){
+			if (s1[r-1]==s2[c-1]){
+				arr[d--]=s2[c-1];
+				r--;
+				c--;
+			}
+			else{
+				if (mat[r-1][c]>mat[r][c-1]){
+					r--;
+				}
+				else{
+					c--;
+				}
+			}
+	}
+	cout<<"Path"<<endl;
+	cout<<arr<<endl;
 	cout<<endl;
 	return mat[rows-1][cols-1];
+	
 }
 int main(){
 	cout<<"Enter the first string:";
@@ -45,5 +68,5 @@ int main(){
 	cout<<"Enter the second string:";
 	string s2;
 	cin>>s2;
-	cout<<lcs(s1,s2);
+	lcs(s1,s2);
 }
