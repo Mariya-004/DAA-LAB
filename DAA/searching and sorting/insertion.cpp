afe8d7e4a0e3b3659c4b16465d07ab761b91a343
Mariya-004 <mariyajyothy04@gmail.com>
2024-11-20 00:34:10 +0530

    Update insertion.cpp

--- DAA/searching and sorting/insertion.cpp
+++ DAA/searching and sorting/insertion.cpp
@@ -4,14 +4,12 @@ void insertion(int A[],int len){
 	int i=1;
 	while(i<len){
 		int j=i-1;
-		while (j<len){
-			if (A[j] >A[i]){
-				int t=A[j];
-				A[j]=A[i];
-				A[i]=t;
-			}
-			j++;
+		int key=A[i];
+		while (j>=0 and A[j]>key){
+			A[j+1]=A[j];
+			j--;
 		}
+		A[j+1]=key;
 		i++;
 	}
 }
@@ -24,4 +22,8 @@ int main(){
 	}
 	cout<<endl;
 }
+		
+
+			
+	
 		
